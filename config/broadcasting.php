64a16f6a35a0b69b289a82dde2edb83a030dd630
Harvey Asprilla <kikiyam_8@hotmail.com>
2024-10-07 20:20:51 -0500

    broad

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -1,4 +1,38 @@
-'options' => [
-  'cluster' => 'us2',
-  'useTLS' => true
-],
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+return [
+
+    'default' => env('BROADCAST_DRIVER', 'null'),
+
+    'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+            ],
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+    ],
+
+];
+
