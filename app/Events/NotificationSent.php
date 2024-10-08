bfa4817caac1f3ba5176a56c86a068b3c30e382a
Harvey Asprilla <kikiyam_8@hotmail.com>
2024-10-07 19:19:37 -0500

    implements

--- app/Events/NotificationSent.php
+++ app/Events/NotificationSent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationSent
+class NotificationSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
