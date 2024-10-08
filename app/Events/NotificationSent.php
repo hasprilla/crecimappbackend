99089453dbfa17a2c9bf2ed0e794afe29be83e83
Harvey Asprilla <kikiyam_8@hotmail.com>
2024-10-07 19:24:59 -0500

    delete int

--- app/Events/NotificationSent.php
+++ app/Events/NotificationSent.php
@@ -20,11 +20,7 @@ class NotificationSent implements ShouldBroadcast
     {
         $this->message = $message;
     }
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
+   
     public function broadcastOn()
     {
         return new Channel('notifications');
